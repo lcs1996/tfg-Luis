1ede1ea45529a06b9c2fe83e870aa8909a8dd07c
lcs1996 <49436820+lcs1996@users.noreply.github.com>
2021-10-19 13:09:09 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,39 @@
+<?php 
+require 'logica/conexion.php';
+session_start();
+if (isset($_SESSION['username'])) {
+  header("location: layouts/paginaprincipal.php");
+}else {
+  ?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="layouts/estilo.css">
+  <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+  <title>Web</title>
+</head>
+
+<body>
+  <header>
+  <?php require('layouts/header.php') ?>
+  </header>
+
+  <?php
+  require('logica/login.php');
+  ?>
+  </body>
+
+</html>
+  <?php
+}
+?>
+  
---
+++ register.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="css/bootstrap.min.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/bootstrap-theme.min.css" crossorigin="anonymous">
+  <link rel="stylesheet" href="layouts/estilo.css">
+  <script src="js/bootstrap.min.js" crossorigin="anonymous"></script>
+
+  <title>Web</title>
+</head>
+
+<body>
+  <header>
+  <?php require('layouts/header.php') ?>
+  </header>
+  
+
+<?php require('logica/registro.php') ?>
+
+  
+</body>
+
+</html>
