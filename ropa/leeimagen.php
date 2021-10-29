0d8f7b8008b2c50588c7fb75bb96b74e7766d647
lcs1996 <49436820+lcs1996@users.noreply.github.com>
2021-10-29 14:24:36 +0200

    Add files via upload

--- ropa/leeimagen.php
+++ ropa/leeimagen.php
@@ -1,174 +1,107 @@
-<!doctype html>
-<html lang="en">
+<!--Main layout-->
+  <main class="my-5">
+    <div class="container">
+      <!--Section: Content-->
+      <section class="text-center">
+        <h4 class="mb-5"><strong>Latest posts</strong></h4>
 
-<head>
-  <title>Lee Imagen</title>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="../funciones/estilo.css">
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-</head>
-<style>
-  .close {
-    font-size: 1.5rem;
-  }
-
-  .col-12 img {
-    opacity: 0.7;
-    cursor: pointer;
-    margin: 2rem;
-    width: 100%;
-  }
-
-  .col-12 img:hover {
-    opacity: 1;
-    box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-
-  }
-</style>
-
-<?php
-$usuario = $_SESSION['username'];
-?>
-
-<body>
-  <label>
-    <h6>Buscar articulos por categoria:</h6>
-  </label>
-  <form method="post" enctype="multipart/form-data">
-    <div class="form-group">
-      <select name="ropa" class="custom-select">
-        <option value="camiseta">Camiseta</option>
-        <option value="pantalones">Pantalones</option>
-        <option value="sudadera">Sudadera</option>
-        <option value="zapatillas">Zapatillas</option>
-        <option value="accesorio">Accesorio</option>
-      </select>
-    </div>
-    <div class="form-group">
-      <button type="submit" class="btn btn-primary" name="buscar">Búsqueda</button>
-    </div>
-  </form>
-  <hr>
-  <div class="container">
-    <div class="row d-flex flex-wrap align-items-center" data-target="#lightbox">
-
-      <?php
-      if (isset($_POST['buscar'])) {
-        $con = mysqli_connect("localhost", "root", "", "tfg");
-        $ropa = $_POST['ropa'];
-        $query = "SELECT * FROM imagenes where email='" . $usuario . "' and categoria='" . $ropa . "'";
-        $res = mysqli_query($con, $query);
-        while ($row = mysqli_fetch_assoc($res)) {
-          $tipo = $row['tipo'];
-          $imagen = $row['imagen'];
-      ?>
-
-<div class="col-12 col-md-6 col-lg-3">
-            <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-lg" role="document">
-                <!--Content-->
-                <?php
-                $conex = mysqli_connect("localhost", "root", "", "tfg");
-                $ropa = $_POST['ropa'];
-                $consulta = "SELECT * FROM imagenes where email='" . $usuario . "' and categoria='" . $ropa . "'";
-                $result = mysqli_query($conex, $consulta);
-                 while ($fil = mysqli_fetch_assoc($result)) {
-                   $a=$fil['tipo'];
-                   $b = $fil['imagen'];
-                ?>
-                <div class="modal-content">
-                  <!--Body-->
-                  <div class="modal-body mb-0 p-0">
-                    <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-                      <img class="embed-responsive-item" src="data:<?php echo $a; ?>;base64,<?php echo  base64_encode($b); ?>"></img>
-                    </div>
-                  </div>
-                  <!--Footer-->
-                  <div class="modal-footer justify-content-center">
-                    <form action="../ropa/add_calendario.php?id=<?php echo $fil['id']; ?>" method="POST">
-                      <label for="cal">Fecha: </label>
-                      <input type="date" id="cal" name="fecha">
-                      <button type="submit" class="btn btn-primary" name="anadir"> Añadir</button>
-                    </form>
-
-                    <a type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" href="../ropa/eliminar_img.php?id=<?php echo $row['id']; ?>">Eliminar</a>
-                    <a type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" href="../ropa/add_fav.php?id=<?php echo $row['id']; ?>">Añadir favorito</a>
-                    <button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
-                  </div>
-                </div>
-                <?php
-                 }
-                ?>
-                <!--/.Content-->
+        <div class="row">
+          <div class="col-lg-6 mb-4">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="https://mdbootstrap.com/img/new/standard/nature/184.jpg" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Post title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the
+                  card's content.
+                </p>
+                <a href="#!" class="btn btn-primary">Read</a>
               </div>
             </div>
-            <a><img data-toggle="modal" data-target="#modal1" class="img-fluid z-depth-1" src="data:<?php echo $tipo; ?>;base64,<?php echo  base64_encode($imagen); ?>" data-target="#indicators" data-slide-to="0" alt="" /></a>
           </div>
-        <?php
-        }
-      } else {
-        $con = mysqli_connect("localhost", "root", "", "tfg");
-        $query = "SELECT * FROM imagenes where email='" . $usuario . "';";
-        $res = mysqli_query($con, $query);
-        while ($row = mysqli_fetch_assoc($res)) {
-          $tipo = $row['tipo'];
-          $imagen = $row['imagen'];
-        ?>
-          <div class="col-12 col-md-6 col-lg-3">
-            <div class="modal fade" id="modal1" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-              <div class="modal-dialog modal-lg" role="document">
-                <!--Content-->
-                <?php
-                $conex = mysqli_connect("localhost", "root", "", "tfg");
-                $consulta = "SELECT * FROM imagenes where email='" . $usuario . "';";
-                $result = mysqli_query($conex, $consulta);
-                 while ($fil = mysqli_fetch_assoc($result)) {
-                   $a=$fil['tipo'];
-                   $b = $fil['imagen'];
-                ?>
-                <div class="modal-content">
-                  <!--Body-->
-                  <div class="modal-body mb-0 p-0">
-                    <div class="embed-responsive embed-responsive-16by9 z-depth-1-half">
-                      <img class="embed-responsive-item" src="data:<?php echo $a; ?>;base64,<?php echo  base64_encode($b); ?>" data-target="#indicators" data-slide-to="0" alt="" ></img>
-                    </div>
-                  </div>
-                  <!--Footer-->
-                  <div class="modal-footer justify-content-center">
-                    <form action="../ropa/add_calendario.php?id=<?php echo $fil['id']; ?>" method="POST">
-                      <label for="cal">Fecha: </label>
-                      <input type="date" id="cal" name="fecha">
-                      <button type="submit" class="btn btn-primary" name="anadir"> Añadir</button>
-                    </form>
 
-                    <a type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" href="../ropa/eliminar_img.php?id=<?php echo $row['id']; ?>">Eliminar</a>
-                    <a type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" href="../ropa/add_fav.php?id=<?php echo $row['id']; ?>">Añadir favorito</a>
-                    <button type="button" class="btn btn-outline-primary btn-rounded btn-md ml-4" data-dismiss="modal">Close</button>
-                  </div>
-                </div>
-                <?php
-                 }
-                ?>
-                <!--/.Content-->
+          <div class="col-lg-6 mb-4">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="https://mdbootstrap.com/img/new/standard/nature/023.jpg" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Post title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the
+                  card's content.
+                </p>
+                <a href="#!" class="btn btn-primary">Read</a>
               </div>
             </div>
-            <a><img data-toggle="modal" data-target="#modal1" class="img-fluid z-depth-1" src="data:<?php echo $tipo; ?>;base64,<?php echo  base64_encode($imagen); ?>" data-target="#indicators" data-slide-to="0" alt="" /></a>
           </div>
-      <?php
-        }
-      }
-      ?>
+        </div>
+
+        <div class="row">
+          <div class="col-lg-6 mb-4">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="https://mdbootstrap.com/img/new/standard/nature/002.jpg" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Post title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the
+                  card's content.
+                </p>
+                <a href="#!" class="btn btn-primary">Read</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-6 mb-4">
+            <div class="card">
+              <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                <img src="https://mdbootstrap.com/img/new/standard/nature/022.jpg" class="img-fluid" />
+                <a href="#!">
+                  <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                </a>
+              </div>
+              <div class="card-body">
+                <h5 class="card-title">Post title</h5>
+                <p class="card-text">
+                  Some quick example text to build on the card title and make up the bulk of the
+                  card's content.
+                </p>
+                <a href="#!" class="btn btn-primary">Read</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--Section: Content-->
+
+      <!-- Pagination -->
+      <nav class="my-4" aria-label="...">
+        <ul class="pagination pagination-circle justify-content-center">
+          <li class="page-item">
+            <a class="page-link" href="#" tabindex="-1" aria-disabled="true">Previous</a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item active" aria-current="page">
+            <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item">
+            <a class="page-link" href="#">Next</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  </div>
-
-  <!-- Optional JavaScript -->
-  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-</body>
-
-</html>
+  </main>
